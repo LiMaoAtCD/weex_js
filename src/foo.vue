3a349952625892f0035f324811f1c9389b32ec8f
liamscofield <limitscofield@gmail.com>
2017-05-12 09:13:00 +0800

    update

--- src/foo.vue
+++ src/foo.vue
@@ -2,26 +2,40 @@
   <div class="wrapper" @click="update">
     <image :src="logoUrl" class="logo"></image>
     <text class="title">Hello {{target}}</text>
+    <text class="baseInfo"> {{ UserInfo }}</text>
   </div>
+
 </template>
 
 <style>
   .wrapper { align-items: center; margin-top: 120px; }
   .title { font-size: 48px; }
   .logo { width: 360px; height: 82px; }
+
+  .baseInfo {
+    font-size: 48px;
+  }
 </style>
 
 <script>
   export default {
     data: {
       logoUrl: 'https://alibaba.github.io/weex/img/weex_logo_blue@3x.png',
-      target: 'World'
+      target: 'World',
+      UserInfo: "userInfo"
     },
     methods: {
       update: function (e) {
         this.target = 'Weex'
         console.log('target:', this.target)
+
+         var baseModule = require('@weex-module/userinfo'); 
+         this.UserInfo = baseModule.getUserInfo();
+        console.log('baseModule get user info'+ this.UserInfo);
       }
     }
   }
+
+ 
+
 </script>
